0c4a0476df9d1c7bf7f2dc3825676405f3f467d4
Geert Koningen <koningen@gmail.com>
2026-08-20 10:25:45 +0200

    Initial setup Next.js application

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Train Journey",
+  description: "What is your destination?",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html>
+      <head>
+        <link rel="icon" type="image/x-icon" href="/app/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="mx-auto max-w-200">
+      <main className="mx-4 bg-white p-10 m-10 rounded-lg shadow-xl">
+        <h1 className="text-2xl font-bold">Initial setup</h1>
+        <p>Welcome to the Train Journey app!</p>
+      </main>
+    </div>
+  );
+}
