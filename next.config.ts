04a829369357560e7df7bd581bfb4167cc38b070
Geert Koningen <koningen@gmail.com>
2026-08-21 10:33:20 +0200

    Fix script mapping, minify JS.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  productionBrowserSourceMaps: true,
+  experimental: {
+    turbopackMinify: true,
+  },
+};
 
 export default nextConfig;
